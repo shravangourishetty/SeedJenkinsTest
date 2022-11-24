c547a35637918acabc123c793f4d45fb1817ed64
shravangourishetty <56544783+shravangourishetty@users.noreply.github.com>
2022-11-24 20:45:14 +0530

    Update SamplePipeline.groovy

--- Shravan-Seed-Jenkins/SamplePipeline.groovy
+++ Shravan-Seed-Jenkins/SamplePipeline.groovy
@@ -1,12 +1,22 @@
 pipeline{
+    agent any 
+    options {
+        timestamps ()
+        skipDefaultCheckout(true)
+    }
     stages{
         stage("First Block"){
+        steps{
+            script{
             echo "This is First block"
-            
-
+            }
+            }
         }
         stage("Second Block"){
-            echo "This is Second Block"
+            steps{
+                script{
+                echo "This is Second Block"
+        }
         }
     }
 }
