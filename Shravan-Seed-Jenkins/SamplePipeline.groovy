12778ff2e02129231f0f3852be600bb836b8f817
ShravanKumar <ShravanKumar@lucidmotors.com>
2022-11-24 20:24:53 +0530

    Adding sample jenkins file

---
+++ Shravan-Seed-Jenkins/SamplePipeline.groovy
@@ -0,0 +1,12 @@
+pipeline{
+    stages{
+        stage("First Block"){
+            echo "This is First block"
+            
+
+        }
+        stage("Second Block"){
+            echo "This is Second Block"
+        }
+    }
+}
